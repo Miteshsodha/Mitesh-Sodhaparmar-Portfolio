c3b10337c9acb030564f1f462e56de12f57addb0
Mitesh Sodhaparmar <169137816+Miteshsodha@users.noreply.github.com>
2026-08-31 20:24:45 +0530

    Update assets.js

--- assets/assets.js
+++ assets/assets.js
@@ -107,9 +107,13 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Python, Java, React.js, Next.js, Node.js' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: [
+    'B.E. in Information Technology | GEC Gandhinagar | CGPA: 8.67',
+    'Diploma in Information Technology | RCTI Sola | CGPA: 8.70',
+    '10th | Sagar Vidyalaya | Percentile: 94.76%'
+  ] },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: ['Tometo - Food Delivery Website','DeepSeek Clone','Spotify Clone'] }
 ];
 
 export const toolsData = [
