819a7b42f7527946c883d0bba587eb4564b28d3a
Mitesh Sodhaparmar <169137816+Miteshsodha@users.noreply.github.com>
2026-08-31 19:46:00 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -27,26 +27,26 @@ const Header = ({ isDarkMode, setIsDarkMode }) => {
       initial={{y:-30, opacity: 0}}
       whileInView={{y: 0,opacity: 1}}
       transition={{duration: 0.8,delay: 0.5}}
-       className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>fullstack web developer based in Ahmedabad. </motion.h1>
+       className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>Full Stack Web Developer based in Ahmedabad.</motion.h1>
       
       <motion.p
       initial={{opacity: 0}}
       whileInView={{opacity: 1}}
       transition={{duration: 0.6,delay: 0.7}}
-       className='max-w-2xl mx-auto font-Ovo'>I am fullstack web developer from Ahmedabad, India persuing BE in IT from GEC Gandhinagar.</motion.p>
+       className='max-w-2xl mx-auto font-Ovo'>I’m a Full Stack Web Developer from Ahmedabad, India, currently pursuing a B.E. in Information Technology at GEC Gandhinagar.</motion.p>
       
       <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
         <motion.a 
         initial={{y:30, opacity: 0}}
       whileInView={{y: 0,opacity: 1}}
       transition={{duration: 0.6,delay: 1}}
-        href="#contact" className={`px-10 py-3 border border-white rounded-full text-white flex items-center gap-2 ${isDarkMode ? "dark:bg-transparent" : "bg-black"}`} >contact me <Image src={assets.right_arrow_white} alt='' className='w-4' /></motion.a>
+        href="#contact" className={`px-10 py-3 border border-white rounded-full text-white flex items-center gap-2 ${isDarkMode ? "dark:bg-transparent" : "bg-black"}`} >Contact Me <Image src={assets.right_arrow_white} alt='' className='w-4' /></motion.a>
         
         <motion.a
         initial={{y:30, opacity: 0}}
       whileInView={{y: 0,opacity: 1}}
       transition={{duration: 0.6,delay: 1.2}}
-         href="/Mitesh_Sodhaparmar_CV.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume <Image src={assets.download_icon} alt='' className='w-4' /></motion.a>
+         href="/Mitesh_Sodhaparmar_CV.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>My Resume <Image src={assets.download_icon} alt='' className='w-4' /></motion.a>
       
       </div>
     
